7b714b9a21574dc18f6c8d681378247a80215643
jabar42 <jaimebarbudomier@gmail.com>
2023-12-23 09:25:59 -0500

    Agregue modificacion con enlace real

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById('miFormulario').addEventListener('submit', function(e) {
     e.preventDefault();
     var userNumber = document.getElementById('inputNumber').value;
-    var baseUrl = "https://tusitio.com/pagina?parametro=";
-    var finalUrl = baseUrl + userNumber;
+    var baseUrl = "https://wa.me/";
+    var text = "?text=Hola%2C%20%C2%BFc%C3%B3mo%20est%C3%A1s%3F";
+    var finalUrl = baseUrl + userNumber + text;
     window.location.href = finalUrl;
 });
